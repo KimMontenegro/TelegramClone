d0e272368a2a1c94b4dc9c83026ef41ac763d8ef
KimMontenegro <mkimberlymae0812@gmail.com>
2020-09-19 04:27:37 +0800

    added addmessage

--- lib/addmessage.dart
+++ lib/addmessage.dart
@@ -10,9 +10,16 @@ class _AddMessageState extends State<AddMessage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('New Message',
-              style: TextStyle(color: Colors.white, fontSize: 18.0),
-            ),
+        title: Text(
+          'New Message',
+          style: TextStyle(color: Colors.white, fontSize: 18.0),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.search),
+          )
+        ],
       ),
       body: Column(
         children: [
@@ -31,12 +38,41 @@ class _AddMessageState extends State<AddMessage> {
             title: Text('New Channel'),
             onTap: () {},
           ),
-          Divider(
+          SizedBox(
+            width: double.infinity,
             height: 20,
-            thickness: 20,
+            child: Container(
+              color: Colors.grey[300],
+              child: Text(
+                'Sorted by last seen time',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600],
+                    fontSize: 15),
+              ),
+            ),
           ),
-
+          Container(
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 28,
+              ),
+              title: Text(
+                'other contact',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('last seen recently'),
+            ),
+          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.blue[600],
+        onPressed: () {},
       ),
     );
   }
